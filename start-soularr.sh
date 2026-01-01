517e1c71b8d13bbfb6aabbae400ac2e9b01d015b
meas <meas@XPS-17.localdomain>
2026-01-01 12:52:56 +0100

    updated soularr scripts to actually run in proper folder

--- start-soularr.sh
+++ start-soularr.sh
@@ -1,1 +1,4 @@
+#!/bin/sh
+
+cd "$(dirname "$0")" || exit 1;
 docker compose --profile=soularr-tools up -d;
--- stop-soularr.sh
+++ stop-soularr.sh
@@ -1,2 +1,6 @@
+#!/bin/sh
+
+cd "$(dirname "$0")" || exit 1;
+
 docker compose stop soularr;
 docker compose stop slskd;
